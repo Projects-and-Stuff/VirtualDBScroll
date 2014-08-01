648e3d368929af3aa83d75ecc795ac3490947414
Projects-and-Stuff <support@projectsandstuff.com>
2014-07-31 20:03:22 -0400

    No significant change

--- TestProject/unit1.pas
+++ TestProject/unit1.pas
@@ -6,8 +6,8 @@ interface
 
 uses
   Classes, SysUtils, db, sqldb, sqlite3conn, FileUtil, Forms, Controls,
-  Graphics, Dialogs, StdCtrls, PASVirtualDBScrollMemo,
-  PASVirtualDBScrollSynEdit;
+  Graphics, Dialogs, StdCtrls, ExtCtrls, PASVirtualDBScrollMemo,
+  PASVirtualDBScrollRichMemo;
 
 type
 
@@ -15,12 +15,16 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
     DataSource1: TDataSource;
-    PASVirtualDBScrollSynEdit1: TPASVirtualDBScrollSynEdit;
+    Label1: TLabel;
     SQLite3Connection1: TSQLite3Connection;
     SQLQuery1: TSQLQuery;
     SQLTransaction1: TSQLTransaction;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure Panel1Resize(Sender: TObject);
   private
     { private declarations }
   public
@@ -67,6 +71,21 @@ begin
   SQLTransaction1.Active := False;
   SQLite3Connection1.Connected := False;
   SQLite3Connection1.Close;
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Panel1Resize(Sender: TObject);
+begin
 
 end;
 
