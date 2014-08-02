6e17ff6da52456909cd17699246863cbacc35f21
Projects-and-Stuff <support@projectsandstuff.com>
2014-08-02 02:10:31 -0400

    Added an embedded panel subcomponent for information display

---
+++ pasembeddedpanel.pas
@@ -0,0 +1,121 @@
+unit PASEmbeddedPanel;
+
+{
+PASVirtualDBScroll Package
+Copyright 2014 Jack D Linke
+
+This package is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This package is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this package. If not, see <http://www.gnu.org/licenses/>.
+}
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Controls, StdCtrls, IDEIntf, PropEdits, typinfo, ExtCtrls;
+
+type
+
+  { TPASEmbeddedLabel }
+
+  TPASEmbeddedPanel = class (TCustomPanel)
+  private
+    { Private declarations }
+    FIsDisplayed : Boolean;
+
+  protected
+    { Protected declarations }
+
+  public
+    { Public declarations }
+    constructor Create(AOwner: TComponent); override;
+
+
+  published
+    property Align;
+    property Alignment;
+    property Anchors;
+    property AutoSize;
+    property BorderSpacing;
+    property BevelInner;
+    property BevelOuter;
+    property BevelWidth;
+    property BidiMode;
+    property BorderWidth;
+    property BorderStyle;
+    property Caption;
+    property ChildSizing;
+    property ClientHeight;
+    property ClientWidth;
+    property Color;
+    property Constraints;
+    property DockSite;
+    property DragCursor;
+    property DragKind;
+    property DragMode;
+    property Enabled;
+    property Font;
+    property FullRepaint;
+    property ParentBidiMode;
+    property ParentColor;
+    property ParentFont;
+    property ParentShowHint;
+    property PopupMenu;
+    property ShowHint;
+    property TabOrder;
+    property TabStop;
+    //property UseDockManager default True;
+    //property Visible;
+    property OnClick;
+    property OnContextPopup;
+    property OnDockDrop;
+    property OnDockOver;
+    property OnDblClick;
+    property OnDragDrop;
+    property OnDragOver;
+    property OnEndDock;
+    property OnEndDrag;
+    property OnEnter;
+    property OnExit;
+    property OnGetSiteInfo;
+    property OnGetDockCaption;
+    property OnMouseDown;
+    property OnMouseEnter;
+    property OnMouseLeave;
+    property OnMouseMove;
+    property OnMouseUp;
+    property OnResize;
+    property OnStartDock;
+    property OnStartDrag;
+    property OnUnDock;
+    property IsDisplayed : Boolean read FIsDisplayed write FIsDisplayed default True;
+
+  end;
+
+implementation
+
+{ TPASEmbeddedLabel }
+
+constructor TPASEmbeddedPanel.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  // Permanently set the values for private properties
+
+
+
+end;
+
+end.
+
